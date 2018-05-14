dcc76344e25bab92460eb37144e42b5edccd58ce
Magdalena Kałamarz <magda.kalamarz@wp.pl>
2018-05-14 23:28:30 +0200

    Add services section

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,22 @@
 'use strict'
 
 let slideIndex = 0;
-showSlides();
 
 function showSlides() {   
-    let i;
     const slides = document.querySelectorAll(".slider__container");
-
+    let i;
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
     slideIndex++;
     
-    if (slideIndex > slides.length) {slideIndex = 1} 
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    } 
     slides[slideIndex-1].style.display = "flex"; 
     setTimeout(showSlides, 6000); 
 }
+showSlides();
+
+
 
